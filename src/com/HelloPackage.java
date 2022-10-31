4614db6b8ee77c4e96eb61c382271553f0a14da7
Buse Cankaya <cankayabuse94@gmail.com>
2022-10-31 20:17:36 +0200

    first commit

---
+++ src/com/HelloPackage.java
@@ -0,0 +1,13 @@
+package com;
+
+public class HelloPackage {
+
+	public static void main(String[] args) {
+		// Burda parent package içinde başka şeyler oluşturabildiğimizi hgöstermek için oluşturdum.
+		
+		
+		System.out.println("Hello Package!");
+
+	}
+
+}
